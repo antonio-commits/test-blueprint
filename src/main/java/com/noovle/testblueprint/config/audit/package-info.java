c65ade73118e770d050e0f3aa8ecce4245a2e627
Antonio Ventimiglia <antonio.ventimiglia@noovle.com>
2020-11-20 15:05:40 +0100

    Initial version of testblueprint generated by JHipster-6.9.0 with blueprints: entando-6.2.0

---
+++ src/main/java/com/noovle/testblueprint/config/audit/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Audit specific code.
+ */
+package com.noovle.testblueprint.config.audit;
---
+++ src/main/java/com/noovle/testblueprint/config/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Spring Framework configuration files.
+ */
+package com.noovle.testblueprint.config;
